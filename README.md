d72e76a7005d99ee686dad5fabffa61f44b799b9
Galicarnax <44889241+Galicarnax@users.noreply.github.com>
2019-10-20 12:15:19 +0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,109 @@
+# dcvim
+An extension for `Double Commander` file manager to provide Vim-like key bindings
+
+While there is a number of console file managers with Vim-like keybindings (e.g., `ranger` and `vifm`), it seems that no GUI analogs exist. The `dcvim` extension allows to use `Double Commander (DC)`, a cross-platform GUI file manager, in Vim-like manner to the extent allowed by the application's API. Since DC does not expose some of its functionality (e.g., basic cursor movements) to its scripts, `dcvim` relies on external key emulation utilities (`xdotool` in case of Unix/Linux, and an *ad hoc* self-made small utility in case of MS Windows).
+
+Currently, `dcvim` works in `Unix/Linux` and `MS Windows` only.
+
+If you want to keep your custom shortcuts along with `dcvim` keybindings, you should copy the contents of `keys.xml` into `shortcuts.csf` instead of using `dcvim.csf` (but make sure there is no interference).
+
+# Install
+### Linux
+- Install `xdotool` utility (e.g., in Ubuntu: `sudo apt install xdotool`)
+- [Make sure](https://doublecmd.github.io/doc/en/lua.html#dllrequired) Lua library is installed  (e.g., in Ubuntu: `sudo apt install liblua5.1-0`)
+- Copy/clone `dcvim` directory into DC's [config directory](https://doublecmd.github.io/doc/en/configxml.html)
+- Copy or move `dcvim.scf` from `dcvim` one level up into DC's config directory
+- Configure DC:
+	- `Options->Keys`: set `Typing->Letters` to `None`  
+	- `Options->Keys->Hotkeys`: select `dcvim.scf` as the `Shortcut file`
+
+### MS Windows
+- [Make sure](https://doublecmd.github.io/doc/en/lua.html#dllrequired) `lua5.1.dll` is present in your DC folder
+- Copy/clone `dcvim` directory into DC's [config directory](https://doublecmd.github.io/doc/en/configxml.html)
+- Copy the contents of `dcvim\mswin\toolbar_items.xml` into `doublecmd.xml`, into the section enclosed by `<Toolbars><MainToolbar><Row>` tags (before copying, make sure DC is not running). <span style="font-size:0.9em;">[Note that this will introduce several buttons in the toolbar menu. They are used to launch key emulation utility from Lua scripts, since direct call of such utility from Lua does not work properly in Windows.]</span>
+- (Optional) If you are fussy about running extraneous executables, you may prefer to build `dcvim\mswin\keyemulate.exe` from C++ code yourself.
+- Copy or move `dcvim.scf` from `dcvim` one level up into DC's config directory
+- Configure DC:
+	- `Options->Keys`: set `Typing->Letters` to `None`  
+	- `Options->Keys->Hotkeys`: select `dcvim.scf` as the `Shortcut file` 
+
+
+# Keybindings
+
+Keys | Description 
+---|---
+`k` | move cursor up
+`j` | move cursor down
+`h` | move to parent directory
+`l`| enter subdirectory or archive[<sup>1</sup>](#1)
+`gg` | go to top of list
+`G` | go to bottom of list
+`gj` | move page down
+`gk` | move page up
+`]` | move 5 entries down
+`[` | move 5 entries up
+`H` | move back in history
+`L` | move forward in history
+`f{char}` | jump to first entry which starts with the character `{char}`
+`;` | jump to next entry in the search initiated with `f{char}` 
+`,` | jump to next entry in the search initiated with `f{char}` (exclude directories)
+`/` | quick search
+`gh` | go to home directory
+`g/` | go to root directory
+`:` | command line
+`vv` | select (mark) all entries
+`vu` | unselect all entries
+`va`, `v+` | add entries to selection using masks
+`vd`, `v-` | remove entries from selection using masks
+`ve` | select files with the same extension as that of the file under cursor
+`vi` | invert selection
+`K` | move cursor up and toggle selection of entries
+`J` | move cursor down and toggle selection of entries
+`yy` | yank (copy) to clipboard
+`yd` | cut to clipboard
+`yf` | copy full paths to clipboard
+`yn` | copy names only
+`pp` | put (paste) from clipboard
+`cp` | copy into opposite panel (without confirmation)
+`cm` | move into opposite panel (without confirmation)
+`dd` | delete to trash
+`DD` | delete permanently 
+`sn` | sort by name / toggle direction
+`ss` | sort by size / toggle direction
+`sd` | sort by date / toggle direction
+`se` | sort by extension / toggle direction
+`sa` | sort by attribute / toggle direction
+`m{char}` | bookmark current location[<sup>2</sup>](#2)
+`'{char}`, `` `{char}`` | go to bookmarked location
+`''`, ` `` ` | show DC's directory hotlist 
+`nf` | new file
+`nd` | new directory
+`nn` | new network connection
+`x` | close network connection
+`gf` | advanced search
+`g.` | toggle hidden files
+`gb` | toggle flat view of directory contents
+`gy` | sync directories
+`gi` | show history
+`rr` | rename 
+`ri` | rename (put cursor in front of file name)
+`ra` | rename (put cursor after file name)[<sup>3</sup>](#3)
+`re` | rename (put cursor after file extension)
+`rm` | multi-rename tool
+`gs` | count sizes of subdirectories
+`z` | hide/show right (or lower) panel
+`>` | increment size of the left/upper panel
+`<` | decrement size of the left/upper panel
+`u` | swap panels
+`R` | refresh
+`e` | edit file
+`go` | open Options window
+`Esc` | cancel previous "modifier"-key - `g`, `f`, `y`, etc. (back to "normal" mode)
+`Q` | quit
+
+
+<a class="anchor" id="1">1</a>. Unlike in `ranger` and `vifm`, `l` does not open files, only directories. Binding it to open files is not a good idea as it often leads to casual launch of apps while navigating. However, this might be Ok if a file is an archive, in which case DC enters it as if it was a directory. However, with DC APIs it seems impossible to distinguish files and directories within archives, as well as within remote (FTP) paths, so in these cases `l` acts as an Open action for files as well.
+
+<a class="anchor" id="2">2</a>. These bookmarks are different from DC's own directory hotlist. NB: when bookmarking a remote (FTP) location, DC API does not provide WFX-plugin prefix in the path (although internally it works correct for DC's directory hotlist). So to be able to jump to that bookmark, you have to manually add a prefix (e.g., wfx://FTP/...) for the corresponding entry in `dcvim/vars/bookmarks`.
+
+<a class="anchor" id="3">3</a>. This key binding assumes you have set the option `Select file name without extension when renaming` in `Options->File Operations->User Interface` (if not, `ra` acts as `re`)
---
+++ dcvim.lua
@@ -0,0 +1,407 @@
+local param = {...}
+
+-- script accepts three params: the key, %dc_config_path% and path of current filer/dir
+if #param < 4 then
+    DC.LogWrite("dcvim script called with less than 3 parameters", 2, true, false)
+    return
+end
+
+local key = param[1]
+local delim = SysUtils.PathDelim
+local file_mode = param[2] .. delim .. "dcvim" .. delim .. "vars" .. delim .. "mode"
+
+-- <ESC> hit: go to normal mode
+if key == "esc" then
+    io.open(file_mode, "w"):close()
+    return
+end
+
+function emulateKey(ekey, count)
+    -- is there a better way to determine from the script in which OS type we are?
+    if delim == "/" then
+        local tkey = ekey
+        -- xdotool uses quirky names for PageUp and PageDown
+        if ekey == "PageUp" then tkey = "Page_Up" end
+        if ekey == "PageDown" then tkey = "Page_Down" end
+        local str = tkey
+        for i = 2, count, 1 do
+            str = str .. " " .. tkey
+        end
+        os.execute("xdotool key --delay 0 " .. str)
+    else
+        if count > 1 then
+            DC.ExecuteCommand("cm_ExecuteToolBarItem", "ToolItemID={" .. ekey .. "-" .. count .. "-Key-Emulator}")
+        else
+            DC.ExecuteCommand("cm_ExecuteToolBarItem", "ToolItemID={" .. ekey .. "-Key-Emulator}")
+        end
+    end
+    return
+end
+
+local mode = nil
+local mfile = io.open(file_mode, "r")
+if mfile ~= nil then
+    mode = mfile:read("*l")
+    mfile:close()
+end
+
+if mode == nil or mode == "" then
+    
+    if key == "f" or key == "g" or key == "s" or key == "d"
+        or key == "y"or key == "p" or key == "v" or key == "m" or key == "n"
+        or key == "'" or key == "`" or key == "c" or key == "D" or key == "r" then
+        
+        mfile = io.open(file_mode, "w")
+        mfile:write(key)
+        mfile:close()
+        
+        if key == "f" then
+            local file_find_sym = param[2] .. delim .. "dcvim" .. delim .. "vars" .. delim .. "find_sym"
+            local ffile = io.open(file_find_sym, "w")
+            ffile:write("")
+            ffile:close()
+        end
+        
+    elseif key == "j" then
+        emulateKey("Down", 1)
+        
+    elseif key == "h" then
+        DC.ExecuteCommand("cm_ChangeDirToParent")
+        
+    elseif key == "k" then
+        emulateKey("Up", 1)
+        
+    elseif key == "[" then
+        emulateKey("Up", 5)
+        
+    elseif key == "]" then
+        emulateKey("Down", 5)
+        
+    elseif key == "u" then
+        DC.ExecuteCommand("cm_Exchange")
+        
+    elseif key == ":" then
+        DC.ExecuteCommand("cm_FocusCmdLine")
+        
+    elseif key == "H" then
+        DC.ExecuteCommand("cm_ViewHistoryPrev")
+        
+    elseif key == "L" then
+        DC.ExecuteCommand("cm_ViewHistoryNext")
+        
+    elseif key == "/" then
+        DC.ExecuteCommand("cm_QuickSearch")
+        
+    elseif key == "G" then
+        DC.ExecuteCommand("cm_GoToLastFile")
+        
+    -- elseif key == "R" then
+        -- DC.ExecuteCommand("cm_RenameOnly")
+        
+    elseif key == "l" then
+        local fileattr = SysUtils.FileGetAttr(param[3])
+        -- apply Open command only if directory is under cursor
+        if fileattr > 0 then
+            if math.floor(fileattr / 0x00000010) % 2 ~= 0 then
+                DC.ExecuteCommand("cm_Open")
+            else
+                -- allow enter archives. Scan doublecmd.xml for matching extensions between ArchiveExt tags (an ad-hoc solution)
+                local ext = SysUtils.ExtractFileExt(param[3])
+                if ext == nil or ext == "" then return end
+                local file_dc_config = param[2] .. delim .. "doublecmd.xml"
+                local fp = io.open(file_dc_config, "r")
+                if fp ~= nil then
+                    -- escape minus and plus symbols which may occur in extension (think '.sublime-package')
+                    ext = ext:sub(2):gsub("([%-%+])", "%%%1")
+                    for line in fp:lines() do
+                        if line:match("<ArchiveExt>" .. ext .. "</ArchiveExt>") then
+                            DC.ExecuteCommand("cm_Open")
+                            break
+                        end
+                    end
+                    fp:close()
+                end
+            end
+        else
+            -- something different - probably we are within archive or a network connection
+            -- in this case it seems there is no way to distinguish between directories and files using DC commands,
+            -- so just trigger Open action for any entry
+            DC.ExecuteCommand("cm_Open")
+        end
+        
+    elseif key == "," then
+        local file_find_sym = param[2] .. delim .. "dcvim" .. delim .. "vars" .. delim .. "find_sym"
+        local ffile = io.open(file_find_sym, "r")
+        if ffile == nil then return end
+        local prefix = ffile:read("*l")
+        ffile:close()
+        if prefix == nil or prefix == "" then return end
+        DC.ExecuteCommand("cm_QuickSearch", "search=on", "direction=next", "files=on", "directories=off", "text=" .. prefix)
+        DC.ExecuteCommand("cm_QuickSearch", "search=off")
+        
+    elseif key == ";" then
+        local file_find_sym = param[2] .. delim .. "dcvim" .. delim .. "vars" .. delim .. "find_sym"
+        local ffile = io.open(file_find_sym, "r")
+        if ffile == nil then return end
+        local prefix = ffile:read("*l")
+        ffile:close()
+        if prefix == nil or prefix == "" then return end
+        DC.ExecuteCommand("cm_QuickSearch", "search=on", "direction=next", "files=on", "directories=on", "text=" .. prefix)
+        DC.ExecuteCommand("cm_QuickSearch", "search=off")
+        
+    elseif key == "e" then
+        DC.ExecuteCommand("cm_Edit")
+
+    elseif key == "R" then
+        DC.ExecuteCommand("cm_Refresh")
+        
+    elseif key == "z" then
+        -- DC.LogWrite("DDDDDD", 0, true, false)
+        local fn = param[2] .. delim .. "dcvim" .. delim .. "vars" .. delim .. "panel"
+        local fp = io.open(fn, "r")
+        local w = nil
+        if fp ~= nil then
+            w = fp:read("*l")
+            fp:close()
+        end
+        if w == nil or w == "" or w == "0" then
+            w = 100
+            fp = io.open(fn, "w")
+            fp:write("1")
+            fp:close()
+        else
+            fp = io.open(fn, "w")
+            fp:write("0")
+            fp:close()
+            fn = param[2] .. delim .. "dcvim" .. delim .. "vars" .. delim .. "panelw"
+            fp = io.open(fn, "r")
+            if fp ~= nil then
+                w = fp:read("*l")
+                fp:close()
+            end
+            if not w:match("%d+") then
+                w = 50
+            end
+        end
+        -- DC.LogWrite("w: " .. w, 0, true, false)
+        DC.ExecuteCommand("cm_PanelsSplitterPerPos", "splitpct=" .. w)
+        
+    elseif key == ">" then
+        local w = 50
+        local fn = param[2] .. delim .. "dcvim" .. delim .. "vars" .. delim .. "panelw"
+        local fp = io.open(fn, "r")
+        if fp ~= nil then
+            w = fp:read("*l")
+            if not w:match("%d+") then w = 50 end
+            fp:close()
+        end
+        w = w + 1
+        DC.ExecuteCommand("cm_PanelsSplitterPerPos", "splitpct=" .. w)
+        fp = io.open(fn, "w")
+        fp:write(w)
+        fp:close()
+        
+    elseif key == "<" then
+        local w = 50
+        local fn = param[2] .. delim .. "dcvim" .. delim .. "vars" .. delim .. "panelw"
+        local fp = io.open(fn, "r")
+        if fp ~= nil then
+            w = fp:read("*l")
+            if not w:match("%d+") then w = 50 end
+            fp:close()
+        end
+        w = w - 1
+        DC.ExecuteCommand("cm_PanelsSplitterPerPos", "splitpct=" .. w)
+        fp = io.open(fn, "w")
+        fp:write(w)
+        fp:close()
+
+    elseif key == "x" then
+    	DC.ExecuteCommand("cm_NetworkDisconnect")
+
+    elseif key == "Q" then
+    	DC.ExecuteCommand("cm_Exit")
+
+    end
+
+    return
+end
+
+
+-- clear the mode file
+io.open(file_mode, "w"):close()
+
+
+if mode == "f" then
+    local file_find_sym = param[2] .. delim .. "dcvim" .. delim .. "vars" .. delim .. "find_sym"
+    local ffile = io.open(file_find_sym, "w")
+    ffile:write(key)
+    ffile:close()
+    DC.ExecuteCommand("cm_QuickSearch", "search=on", "direction=first", "files=on", "directories=on", "text=" .. key)
+    DC.ExecuteCommand("cm_QuickSearch", "search=off")
+    
+elseif mode == "g" then
+    if key == "g" then
+        DC.ExecuteCommand("cm_GotoFirstFile")
+    elseif key == "j" then
+        emulateKey("PageDown", 1)
+    elseif key == "k" then
+        emulateKey("PageUp", 1)
+    elseif key == "f" then
+        DC.ExecuteCommand("cm_Search")
+    elseif key == "h" then
+        DC.ExecuteCommand("cm_ChangeDirToHome")
+    elseif key == "i" then
+        DC.ExecuteCommand("cm_DirHistory")
+    elseif key == "y" then
+        DC.ExecuteCommand("cm_SyncDirs")
+    elseif key == "." then
+        DC.ExecuteCommand("cm_ShowSysFiles")
+    elseif key == "b" then
+        DC.ExecuteCommand("cm_FlatView")
+    elseif key == "s" then
+        DC.ExecuteCommand("cm_CountDirContent")
+    elseif key == "o" then
+        DC.ExecuteCommand("cm_Options")
+    elseif key == "/" then
+        DC.ExecuteCommand("cm_ChangeDirToRoot")
+    end
+    
+
+elseif mode == "s" then
+    if key == "n" then
+        DC.ExecuteCommand("cm_SortByName")
+    elseif key == "s" then
+        DC.ExecuteCommand("cm_SortBySize")
+    elseif key == "e" then
+        DC.ExecuteCommand("cm_SortByExt")
+    elseif key == "d" then
+        DC.ExecuteCommand("cm_SortByDate")
+    elseif key == "a" then
+        DC.ExecuteCommand("cm_SortByAttr")
+    end
+        
+elseif mode == "D" then
+    if key == "D" then
+        DC.ExecuteCommand("cm_Delete", "confirmation=no", "trashcan=off")
+    end
+        
+elseif mode == "d" then
+    if key == "d" then
+        DC.ExecuteCommand("cm_Delete", "confirmation=no", "trashcan=on")
+    end
+
+elseif mode == "y" then
+    if key == "y" then
+        DC.ExecuteCommand("cm_CopyToClipboard")
+    elseif key == "n" then
+        DC.ExecuteCommand("cm_CopyNamesToClip")
+    elseif key == "f" then
+        DC.ExecuteCommand("cm_CopyFullNamesToClip")
+    -- elseif key == "v" then
+        -- DC.ExecuteCommand("cm_Rename", "confirmation=off")
+    elseif key == "d" then
+        DC.ExecuteCommand("cm_CutToClipboard")
+    -- elseif key == "c" then
+        -- DC.ExecuteCommand("cm_CopyNoAsk")
+    end
+    
+elseif mode == "p" then
+    if key == "p" then
+        DC.ExecuteCommand("cm_PasteFromClipboard")
+    end
+    
+elseif mode == "v" then
+    if key == "v" then
+        DC.ExecuteCommand("cm_MarkMarkAll")
+    elseif key == "u" then
+        DC.ExecuteCommand("cm_MarkUnmarkAll")
+    elseif key == "i" then
+        DC.ExecuteCommand("cm_MarkInvert")
+    elseif key == "a" or key == "+" then
+        DC.ExecuteCommand("cm_MarkPlus")
+    elseif key == "d"  or key == "-" then
+        DC.ExecuteCommand("cm_MarkMinus")
+    elseif key == "e" then
+        -- cm_MarkCurrentExtension selects everything if cursor is on a directory or a file without extension - avoid that
+        local ext = SysUtils.ExtractFileExt(param[3])
+        if ext ~= nil and ext ~= "" then
+            DC.ExecuteCommand("cm_MarkCurrentExtension")
+        end
+    end
+    
+elseif mode == "m" then
+    if key:len() ~= 1 then return end
+    if key:match("%a") or key:match("%d") then
+        local curdir = param[4]
+        local file_bookmarks = param[2] .. delim .. "dcvim" .. delim .. "vars" .. delim .. "bookmarks"
+        local fp = io.open(file_bookmarks, "r")
+        local content = {}
+        if fp ~= nil then
+            for line in fp:lines() do
+                if key ~= line:sub(1, 1) then
+                    content[#content + 1] = line
+                end
+            end
+            fp:close()
+        end
+        fp = io.open(file_bookmarks, "w")
+        if (#content > 0) then
+            for i = 1, #content do
+                fp:write(string.format("%s\n", content[i]))
+            end
+        end
+        fp:write(key .. " " .. curdir .. "\n")
+        fp:close()
+    end
+    
+elseif mode == "'" or key == "`" then
+    if key == "'" or key == "`" then
+        DC.ExecuteCommand("cm_DirHotList")
+    elseif key:match("%a") or key:match("%d") then
+        local file_bookmarks = param[2] .. delim .. "dcvim" .. delim .. "vars" .. delim .. "bookmarks"
+        local fp = io.open(file_bookmarks, "r")
+        if fp ~= nil then
+            for line in fp:lines() do
+                if key == line:sub(1, 1) then
+                    DC.ExecuteCommand("cm_ChangeDir", "activepath=" .. line:sub(3))
+                    break
+                end
+            end
+            fp:close()
+        end
+    end
+    
+elseif mode == "c" then
+    if key == "p" then
+        DC.ExecuteCommand("cm_CopyNoAsk")
+    elseif key == "m" then
+        DC.ExecuteCommand("cm_Rename", "confirmation=off")
+    end
+
+elseif mode == "r" then
+	if key == "r" then
+        DC.ExecuteCommand("cm_RenameOnly")
+    elseif key == "i" then
+        DC.ExecuteCommand("cm_RenameOnly")
+        emulateKey("Home", 1)
+    elseif key == "a" then
+        DC.ExecuteCommand("cm_RenameOnly")
+        emulateKey("Right", 1)
+    elseif key == "e" then
+        DC.ExecuteCommand("cm_RenameOnly")
+        emulateKey("End", 1)
+    elseif key == "m" then
+        DC.ExecuteCommand("cm_MultiRename")
+    end
+
+elseif mode == "n" then
+	if key == "d" then
+	    DC.ExecuteCommand("cm_MakeDir")
+	elseif key == "f" then
+	    DC.ExecuteCommand("cm_EditNew")
+	elseif key == "n" then
+		DC.ExecuteCommand("cm_NetworkConnect")
+	end
+
+end
